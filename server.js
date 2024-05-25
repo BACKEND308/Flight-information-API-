12383a2cd148359877af4fcedd62e5f28f76455e
mela149 <105762905+mela149@users.noreply.github.com>
2024-05-25 22:44:34 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const connectMongo = require('./db/connectMongo'); // Ensure this properly sets up and exports the MongoDB connection.
+const flightRoutes = require('./routes/flight.routes'); // Adjust path if necessary
 
 const app = express();
 
@@ -10,6 +11,9 @@ app.use(express.json()); // for parsing application/json
 // MongoDB Connection
 connectMongo();
 
+// Use the flight routes
+app.use('/api', flightRoutes); // This prefixes '/api' before your routes
+
 // Define a simple route for testing
 app.get('/', (req, res) => {
   res.send('Flight Information API is running!');
@@ -17,7 +21,8 @@ app.get('/', (req, res) => {
 
 
 // Set the port and start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
