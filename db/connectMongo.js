47881d510650cabe0f1bb176d15f599bb4c35164
mela149 <105762905+mela149@users.noreply.github.com>
2024-05-25 22:47:06 +0300

    Update connectMongo.js

--- db/connectMongo.js
+++ db/connectMongo.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
 
 const connectMongo = async () => {
+  console.log('Environment Variable MONGO:', process.env.MONGO); // Add this line to debug
   try {
-    await mongoose.connect(process.env.MONGO, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(process.env.MONGO);
     console.log('MongoDB connected successfully');
   } catch (err) {
     console.error('MongoDB connection error:', err);
