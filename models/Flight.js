8fb47f1cb6932ab34f53d76edcbb7b8a4e0f35c2
mela149 <105762905+mela149@users.noreply.github.com>
2024-05-25 16:29:47 +0300

    Updated Flight.js
    This now reflects the new schema as in the MongoDB

--- models/Flight.js
+++ models/Flight.js
@@ -1,13 +1,39 @@
 const mongoose = require('mongoose');
 
+const connectingFlightInfoSchema = new mongoose.Schema({
+    FlightNumber: String,
+    Company: String,
+    DepartureTime: String
+});
+
+const sharedFlightInfoSchema = new mongoose.Schema({
+    SharedFlightNumber: String,
+    SharedFlightCompany: String,
+    ConnectingFlightInfo: connectingFlightInfoSchema
+});
+
+const vehicleTypeSchema = new mongoose.Schema({
+    Model: String,
+    Capacity: Number,
+    StandardMenu: String
+});
+
+const locationSchema = new mongoose.Schema({
+    Country: String,
+    City: String,
+    AirportName: String,
+    AirportCode: String
+});
+
 const flightSchema = new mongoose.Schema({
-  FlightNumber: { type: String, required: true, unique: true },
-  DepartureAirport: { type: String, required: true },
-  ArrivalAirport: { type: String, required: true },
-  FlightDate: { type: Date, required: true },
-  DepartureTime: { type: String, required: true },
-  ArrivalTime: { type: String, required: true },
-  Price: { type: Number, required: true }
+    FlightNumber: { type: String, required: true, unique: true },
+    Date: { type: Date, required: true },
+    FlightDuration: String,
+    FlightDistance: Number,
+    FlightSource: locationSchema,
+    FlightDestination: locationSchema,
+    VehicleType: vehicleTypeSchema,
+    SharedFlightInfo: sharedFlightInfoSchema
 });
 
 const Flight = mongoose.model('Flight', flightSchema);
