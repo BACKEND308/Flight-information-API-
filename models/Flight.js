1451459b882cbe04c9e5f020da862eedcd083731
mela149 <105762905+mela149@users.noreply.github.com>
2024-05-26 00:50:04 +0300

    Requests are working now

--- models/Flight.js
+++ models/Flight.js
@@ -4,26 +4,26 @@ const connectingFlightInfoSchema = new mongoose.Schema({
     FlightNumber: String,
     Company: String,
     DepartureTime: String
-});
+}, { collection: 'flight_information' });
 
 const sharedFlightInfoSchema = new mongoose.Schema({
     SharedFlightNumber: String,
     SharedFlightCompany: String,
     ConnectingFlightInfo: connectingFlightInfoSchema
-});
+}, { collection: 'flight_information' });
 
 const vehicleTypeSchema = new mongoose.Schema({
     Model: String,
     Capacity: Number,
     StandardMenu: String
-});
+}, { collection: 'flight_information' });
 
 const locationSchema = new mongoose.Schema({
     Country: String,
     City: String,
     AirportName: String,
     AirportCode: String
-});
+}, { collection: 'flight_information' });
 
 const flightSchema = new mongoose.Schema({
     FlightNumber: { type: String, required: true, unique: true },
@@ -34,7 +34,7 @@ const flightSchema = new mongoose.Schema({
     FlightDestination: locationSchema,
     VehicleType: vehicleTypeSchema,
     SharedFlightInfo: sharedFlightInfoSchema
-});
+}, { collection: 'flight_information' }); 
 
 const Flight = mongoose.model('Flight', flightSchema);
 module.exports = Flight;
